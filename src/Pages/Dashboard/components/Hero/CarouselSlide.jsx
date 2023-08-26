73f9167d6a91e58845a108d9f3cf30fe4c8e44b1
Gabriel <murariugabi23@gmail.com>
2023-08-26 18:18:00 +0300

    Carousel is now fully  functional

--- src/Pages/Dashboard/components/Hero/CarouselSlide.jsx
+++ src/Pages/Dashboard/components/Hero/CarouselSlide.jsx
@@ -1,14 +1,13 @@
 import { Link } from "react-router-dom"
 
-const CarouselSlide = ({ name, image, description, id, styles }) => {
-  // console.log(style)
+const CarouselSlide = ({ name, image, shortDescription, id, styles }) => {
 
   return (
     <li className="carousel-slide" style={styles}>
       <img className="category-image" src={image} alt={name} />
       <div>
         <h2>{name}</h2>
-        <p>{description}</p>
+        <p>{shortDescription}</p>
         <Link to={`/productPage/${id}`} className="button-2b">
           see product
         </Link>
@@ -18,31 +17,3 @@ const CarouselSlide = ({ name, image, description, id, styles }) => {
 }
 export default CarouselSlide
 
-// import { Link } from "react-router-dom"
-
-// const CarouselSlide = ({placeItem, position, carouselWidth}) => {
-
-//     const styles = placeItem(position, carouselWidth)
-
-//     const {product} = styles
-
-//     // console.log(product)
-
-//   return (
-//     <li className="carousel-slide" style={styles.styles}>
-//       <img
-//         className="category-image"
-//         src={product.images.productDisplay}
-//         alt={product.name}
-//       />
-//       <div>
-//         <h2>{product.name}</h2>
-//         <p>{product.shortDescription}</p>
-//         <Link to={`/productPage/${product.id}`} className="button-2b">
-//           see product
-//         </Link>
-//       </div>
-//     </li>
-//   )
-// }
-// export default CarouselSlide
--- src/Pages/Dashboard/components/Hero/ProductsDisplay.jsx
+++ src/Pages/Dashboard/components/Hero/ProductsDisplay.jsx
@@ -6,8 +6,10 @@ import "./productDisplayStyles.scss"
 
 const ProductsDisplay = () => {
   const [items, setItems] = useState([])
-  const [moveSlide, setMoveSlide] = useState({})
+  const [isTicking, setIsTicking] = useState(false)
   const [carouselWidth, setCarouselWidth] = useState(0)
+  const [activeIdx, setActiveIdx] = useState(0)
+  const bigLength = items.length
   const trackRef = useRef(null)
 
   const {
@@ -16,12 +18,13 @@ const ProductsDisplay = () => {
     data: products,
   } = useFetch("http://localhost:3000/products?new=true")
 
-  //   const length = products.length
-  // .getBoundingClientRect().width
+  const sleep = (ms = 0) => {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+  }
 
   const placeItem = (position, slideWidth) => {
     const filter = (products) => {
-        return products.filter((product) => product.id !== "item-3")
+      return products.filter((product) => product.id !== "item-3")
     }
 
     const item = {
@@ -36,41 +39,78 @@ const ProductsDisplay = () => {
     return styles
   }
 
-
-
-  // I need to figure out a way to make this work based on current item, to track current position and to add the current position plus the amount of pixels to move
-
-  const moveItem = (slideWidth, operator) => {
-    const operations = {
-      "+": `translateX(${operator}${slideWidth}px)`,
-      "-": `translateX(${operator}${slideWidth}px)`,
-    }
-
-    const item = {
-      styles: {
-        transform: `translateX(-${slideWidth}px)`,
-      },
-
-    }
-
-    
-
-    const { styles } = item
-    console.log(item)
-    setMoveSlide((prev) => ({ ...prev, styles }))
-  }
-
-  // next step is to make functionality to move the slides
-
   useEffect(() => {
     if (trackRef.current) {
       setCarouselWidth(trackRef.current.offsetWidth)
       const keys = Array.from(Array(products.length - 1).keys())
       setItems(keys)
+      
     }
   }, [products])
 
-  // console.log(items)
+  const jumperFactory = (jump) => {
+    return () => {
+      if (!isTicking) {
+        setIsTicking(true)
+        setItems((prev) => {
+          return prev.map((_, i) => {
+            const nextPosition = i + jump
+            const nextJump = jump < 1 ? nextPosition + bigLength : nextPosition
+            return prev[nextJump % bigLength]
+          })
+        })
+      }
+    }
+  }
+
+  console.log("re-render", activeIdx)
+
+  const moveRight = jumperFactory(1)
+  const moveLeft = jumperFactory(-1)
+
+  const handleDotClick = (idx) => {
+    if (idx < activeIdx) {
+      const n = activeIdx - idx
+      const jumpPrevN = jumperFactory(n)
+      jumpPrevN()
+    }
+    if (idx > activeIdx) {
+      const n = idx - activeIdx
+      const jumpNextN = jumperFactory(-n)
+      jumpNextN()
+    }
+  }
+
+  // carousel timed slides
+
+  let carouselTimeout
+
+  const setAutomaticSwitch = () => {
+    carouselTimeout = setInterval(() => {
+      moveLeft()
+    }, 2000)
+  }
+
+
+
+  const resetAutomaticSwitch = () => {
+    clearInterval(carouselTimeout)
+    carouselTimeout = setInterval(() => {
+      moveLeft
+    }, 3000)
+  }
+
+  useEffect(() => {
+    if (isTicking) sleep(300).then(() => setIsTicking(false))
+  }, [isTicking])
+
+  useEffect(() => {
+    setActiveIdx((bigLength - (items[0] % bigLength)) % bigLength)
+    //   setAutomaticSwitch()
+  }, [items])
+
+  useEffect(() => {
+  }, [])
 
   if (isLoading) {
     return <h2>Loading...</h2>
@@ -86,7 +126,7 @@ const ProductsDisplay = () => {
 
         <div className="carousel">
           <div className="carousel-track-container">
-            <button className="carousel-button button-left">
+            <button className="carousel-button button-left" onClick={moveRight}>
               <svg
                 stroke="currentColor"
                 fill="currentColor"
@@ -100,32 +140,24 @@ const ProductsDisplay = () => {
               </svg>
             </button>
 
-            <ul
-              ref={trackRef}
-              className="carousel-track"
-              id="carousel-track"
-            >
+            <ul ref={trackRef} className="carousel-track" id="carousel-track">
               {products
                 .filter((product) => product.id !== "item-3")
                 .map((product, index) => {
-                  // console.log(product)
-                  const { id, description, name, images } = product
+                  const { id, shortDescription, name, images } = product
 
                   return (
                     <CarouselSlide
                       key={id}
-                      {...{ description, name, id }}
+                      {...{ shortDescription, name, id }}
                       image={images.productDisplay}
-                      styles={placeItem(index, carouselWidth)}
+                      styles={placeItem(items[index], carouselWidth)}
                     />
                   )
                 })}
             </ul>
 
-            <button
-              className="carousel-button button-right"
-              onClick={() => moveItem(carouselWidth)}
-            >
+            <button className="carousel-button button-right" onClick={moveLeft}>
               <svg
                 stroke="currentColor"
                 fill="currentColor"
@@ -140,9 +172,19 @@ const ProductsDisplay = () => {
             </button>
 
             <div className="carousel-nav">
-              <button className="carousel-indicator current-slide"></button>
-              <button className="carousel-indicator"></button>
-              <button className="carousel-indicator"></button>
+              {items.slice(0, bigLength).map((position, i) => {
+                return (
+                  <button
+                    key={i}
+                    onClick={() => handleDotClick(i)}
+                    className={
+                      i === activeIdx
+                        ? "carousel-indicator current-slide"
+                        : "carousel-indicator"
+                    }
+                  ></button>
+                )
+              })}
             </div>
           </div>
         </div>
@@ -182,216 +224,3 @@ const ProductsDisplay = () => {
 }
 
 export default ProductsDisplay
-
-// import "./productDisplayStyles.scss"
-
-
-// const ProductsDisplay = () => {
-//   const [items, setItems] = useState([])
-//   const [isTicking, setIsTicking] = useState(false)
-//   const [carouselWidth, setCarouselWidth] = useState(0)
-//   const bigLength = items.length
-//   const trackRef = useRef(null)
-
-//   const {
-//     isLoading,
-//     isError,
-//     data: products,
-//   } = useFetch("http://localhost:3000/products?new=true")
-
-//   //   const length = products.length
-//   // .getBoundingClientRect().width
-
-//   const sleep = (ms = 0) => {
-//     return new Promise((resolve) => setTimeout(resolve, ms))
-//   }
-
-//   const placeItem = (position, slideWidth) => {
-
-//     const filter = (products) => {
-//         return products.filter((product) => product.id !== "item-3")
-//     }
-
-//     const item = {
-//       styles: {
-//         transform: `translateX(${position * slideWidth}px)`,
-//       },
-//       product: filter(products)[items[position]],
-//     }
-
-
-
-//     return item
-//   }
-
-
-//   // next step is to make functionality to move the slides
-
-//   useEffect(() => {
-//     if (trackRef.current) {
-//       setCarouselWidth(trackRef.current.offsetWidth)
-//       const keys = Array.from(Array(products.length - 1).keys())
-//       setItems(keys)
-//     }
-//   }, [products])
-
-
-//   console.log(items)
-
-//   const jumperFactory = (jump) => {
-//     return () => {
-//     //   if (!isTicking) {
-//         // setIsTicking(true)
-//         setItems((prev) => {
-//           return prev.map((_, i) => {
-//             const nextPosition = i + jump
-//             const nextJump = jump < 1 ? nextPosition + bigLength : nextPosition
-//             return prev[nextJump % bigLength]
-//           })
-//         })
-//       }
-//     // }
-//   }
-
-//   const moveRight = jumperFactory(1)
-//   const moveLeft = jumperFactory(-1)
-
-// //    useEffect(() => {
-// //      if (isTicking) sleep(300).then(() => setIsTicking(false))
-// //    }, [isTicking])
-
-
-//   if (isLoading) {
-//     return <h2>Loading...</h2>
-//   }
-//   if (isError) {
-//     return <h2>There was an error</h2>
-//   }
-
-//   return (
-//     <section className="products-display">
-//       <div className="container">
-//         <h2>NEW products</h2>
-
-//         <div className="carousel">
-//           <div className="carousel-track-container">
-//             <button
-//               className="carousel-button button-left"
-//               onClick={() => moveLeft()}
-//               //   moveItem(carouselWidth, "+")
-//             >
-//               <svg
-//                 stroke="currentColor"
-//                 fill="currentColor"
-//                 strokeWidth="0"
-//                 viewBox="150 0 512 512"
-//                 height="1em"
-//                 width="1em"
-//                 xmlns="http://www.w3.org/2000/svg"
-//               >
-//                 <path d="M217.9 256L345 129c9.4-9.4 9.4-24.6 0-33.9-9.4-9.4-24.6-9.3-34 0L167 239c-9.1 9.1-9.3 23.7-.7 33.1L310.9 417c4.7 4.7 10.9 7 17 7s12.3-2.3 17-7c9.4-9.4 9.4-24.6 0-33.9L217.9 256z"></path>
-//               </svg>
-//             </button>
-
-//             <ul
-//               ref={trackRef}
-//               className="carousel-track"
-//               id="carousel-track"
-//             //   style={moveSlide.styles}
-//             >
-//               {items
-//                 .map((product, index) => {
-
-//                   return (
-//                     <CarouselSlide
-//                       key={crypto.randomUUID()}
-//                       placeItem={placeItem}
-//                       position={product}
-//                       carouselWidth={carouselWidth}
-//                     />
-//                   )
-//                 })}
-//             </ul>
-
-//             <button
-//               className="carousel-button button-right"
-//               onClick={() => moveRight()}
-//             >
-//               <svg
-//                 stroke="currentColor"
-//                 fill="currentColor"
-//                 strokeWidth="0"
-//                 viewBox="-150 0 512 512"
-//                 height="1em"
-//                 width="1em"
-//                 xmlns="http://www.w3.org/2000/svg"
-//               >
-//                 <path d="M294.1 256L167 129c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.3 34 0L345 239c9.1 9.1 9.3 23.7.7 33.1L201.1 417c-4.7 4.7-10.9 7-17 7s-12.3-2.3-17-7c-9.4-9.4-9.4-24.6 0-33.9l127-127.1z"></path>
-//               </svg>
-//             </button>
-
-//             <div className="carousel-nav">
-//               <button className="carousel-indicator current-slide"></button>
-//               <button className="carousel-indicator"></button>
-//               <button className="carousel-indicator"></button>
-//             </div>
-//           </div>
-//         </div>
-
-//         <h2>on sale</h2>
-//         <article>
-//           <span className="subtitle">
-//             {" "}
-//             <span>-10%</span>{" "}
-//           </span>
-//           <div>
-//             <h4>xiaomi mi watch</h4>
-//             <Link to="/productPage/item-1" className="button-2">
-//               see product
-//             </Link>
-//           </div>
-//         </article>
-
-//         <article>
-//           <div>
-//             {" "}
-//             <span className="subtitle">
-//               {" "}
-//               <span>-10%</span>{" "}
-//             </span>
-//           </div>
-//           <div>
-//             <h4>garmin vivomove</h4>
-//             <Link to="/productPage/item-7" className="button-2">
-//               see product
-//             </Link>
-//           </div>
-//         </article>
-//       </div>
-//     </section>
-//   )
-// }
-
-// export default ProductsDisplay
-
-
-
-
-// const moveItem = (slideWidth, operator) => {
-//   // setCarouselWidth(prev => prev + slideWidth)
-
-//   const operations = {
-//     "+": `translateX(${slideWidth - slideWidth}px)`,
-//     "-": `translateX(-${slideWidth}px)`,
-//   }
-
-//   const item = {
-//     styles: {
-//       transform: operations[operator],
-//     },
-//   }
-
-//   const { styles } = item
-//   console.log(item)
-//   setMoveSlide((prev) => ({ ...prev, styles }))
-// }
